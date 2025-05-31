24df9e6e50d1d35542d26c14477fbcaa5c064a2f
Dragon_Fly <143097451+gurpreetsingh911@users.noreply.github.com>
2025-05-31 22:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let submit = document.getElementById("btn");
+let output = document.getElementById("output");
+
+submit.addEventListener('click', function(event) {
+event.preventDefault();
+
+	let textVal = text.value;
+	let delayVal = delay.value; 
+	
+	let promise = new Promise((resolve, reject) =>{
+		setTimeout(() => {
+			output.innerHTML = (`${textVal} - ${delayVal}`);
+			resolve();			
+		} delayVal); 
+	
+	}
+
+							  
+		// if(textVal.length == 0 || delayVal <= 0) {
+		// 	output.innerHTML = `Hello`
+		// 	 reject();
+		// }
+		
+	});
+	
+	
+});
