8bbe046a67f5006f6881d5b3181af7338c36fffc
Dragon_Fly <143097451+gurpreetsingh911@users.noreply.github.com>
2025-06-01 11:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,25 +4,30 @@ let submit = document.getElementById("btn");
 let output = document.getElementById("output");
 
 submit.addEventListener('click', function(event) {
-  event.preventDefault();
+event.preventDefault();
 
- let textVal = text.value.trim();
+	let textVal = text.value.trim();
 	let delayVal = delay.value; 
-  let realVal = delay.value *1000; 
-
-  output.innerHTML = ""; // Clear any previous output
-
-  new Promise((resolve, reject) => {
-    if (textVal.length === 0 || delayVal <= 0) {
-      reject("Invalid input");
-    } else {
-      setTimeout(() => {
-        resolve(`${textVal} - ${delayVal}`)
-      }, delayVal);
-    }
-  }).then(result => {
-    output.innerHTML = result;
-  }).catch(() => {
-    output.innerHTML = "";
-  });
+	let realVal = delay.value *1000;  
+	
+  
+	let promise = new Promise((resolve, reject) =>{
+  if(textVal.length == 0 || delayVal <= 0) {
+  	setTimeout(() => {
+	   	output.innerHTML = ``
+      reject();
+      },0);
+      }
+      else{
+		setTimeout(() => {
+    let pass = (`${textVal} - ${delayVal}`);
+    resolve(output.textContent  = (pass));
+				
+	}, realVal); 
+	
+	}
+  
+	});
+	
+	
 });
