a293599b3f3c6b72e4215298d15480ebac6ab33b
Dragon_Fly <143097451+gurpreetsingh911@users.noreply.github.com>
2025-06-01 11:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,38 @@ let delay = document.getElementById("delay");
 let submit = document.getElementById("btn");
 let output = document.getElementById("output");
 
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+submit.addEventListener('click', async function(event) {
+  event.preventDefault();
+
+	let textVal = text.value.trim();
+	let delayVal = delay.value; 
+	let realVal = delay.value *1000;  
+	
+  if (textVal.length === 0 || delayVal <= 0 || isNaN(delayVal)) {
+    output.textContent = 'Please enter valid text and delay (>0).';
+    return;
+  }
+
+  await wait(realVal);
+
+  output.textContent = `${textVal} - ${delayVal}`;
+});
+
+
+/*
+
+
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let submit = document.getElementById("btn");
+let output = document.getElementById("output");
+
 // Helper delay function returning a Promise
 function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -26,3 +58,5 @@ submit.addEventListener('click', async function(event) {
 
   output.textContent = `${textVal} - ${delayVal}`;
 });
+
+*/
